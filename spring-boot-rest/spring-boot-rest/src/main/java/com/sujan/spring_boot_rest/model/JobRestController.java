9f6efa7d3c4571bbde1db7e450126d26672ec2f6
sujan <sujan75349@gmail.com>
2026-02-08 15:55:43 +0530

    initial commit

---
+++ spring-boot-rest/spring-boot-rest/src/main/java/com/sujan/spring_boot_rest/model/JobRestController.java
@@ -0,0 +1,55 @@
+package com.sujan.spring_boot_rest.model;
+
+import com.sujan.spring_boot_rest.service.JobService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+//@Controller
+@RestController /*It's make whole controller Rest like it only return responseBody
+// then you do not need to add @ResponseBody for each request*/
+public class JobRestController {
+
+    @Autowired
+    private JobService service;
+
+    @GetMapping(path = "/jobPosts" , produces = {"application/json"})
+//    @ResponseBody /*By default, @Controller return a view but if you want to return a body add @ResponseBody*/
+    public List<JobPost> getAllJobs(){
+        return service.getAllJobs();
+    }
+
+
+    @GetMapping("/jobPost/{postId}")
+//    @ResponseBody
+    public JobPost getJob(@PathVariable int postId){
+        return service.getJob(postId);
+    }
+
+
+    @PostMapping(path = "/jobPost", consumes  ={"application/xml"})
+//    @ResponseBody
+    public JobPost addJob(@RequestBody JobPost jobPost){
+         service.addJob(jobPost);
+         return service.getJob(jobPost.getPostId());
+    }
+
+    @PutMapping("/jobPost" )
+    public JobPost updatePost(@RequestBody JobPost jobPost){
+        service.updatePost(jobPost);
+        return service.getJob(jobPost.getPostId());
+    }
+
+    @DeleteMapping("jobPost/{postId}")
+    public String deletePost(@PathVariable int postId){
+        service.deletePost(postId);
+        return "Delete Successfully";
+    }
+
+    /*
+    * Consumes and Produce is for content negotiation to restrike user with media format
+    * */
+
+}
---
+++ spring-boot-rest/spring-boot-rest/src/main/java/com/sujan/spring_boot_rest/service/JobService.java
@@ -0,0 +1,37 @@
+package com.sujan.spring_boot_rest.service;
+
+
+import com.sujan.spring_boot_rest.model.JobPost;
+import com.sujan.spring_boot_rest.repo.JobRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+@Service
+public class JobService {
+
+    @Autowired
+    private JobRepo repo;
+
+    public void addJob(JobPost job){
+        repo.addJobs(job);
+    }
+
+    public List<JobPost> getAllJobs(){
+        return repo.getAllJobs();
+    }
+
+    public JobPost getJob(int postId) {
+        return repo.getJob(postId);
+    }
+
+    public void updatePost(JobPost jobPost) {
+        repo.updatePost(jobPost);
+    }
+
+    public void deletePost(int postId) {
+        repo.deletePost(postId);
+    }
+}
